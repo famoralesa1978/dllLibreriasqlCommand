1520408469361e5ce5320779f0e58c86f61d10f8
felipe morales <fmorales_arenas@hotmail.com>
2021-05-17 22:19:11 -0400

    cambio

--- dllLibreriaMysql/clsConector.cs
+++ dllLibreriaMysql/clsConector.cs
@@ -17,13 +17,13 @@ namespace dllConectorMysql
 		 private void Conectar(String strPublicacion)  
 		 {
 
-            string bdProduccion = "Pn6QdbLxN6zYhNuC0AGO9QzP8WL2RI9VHfd/l56YcLkZ1UdzuJNuXq3s7y9ZY3eq6QrxfamnP0GH0FDdEHA6bAWJdHonailm8a5b3eyUw5vuWLyX+mBmFPxKLHFVjRtYm0sjwb1KdqM=";//original 
-            string bdDesarrollo = "qbz4h7qjqnp0/OO4YgugGwzP8WL2RI9VHfd/l56YcLkZ1UdzuJNuXhBXIBpMWzG0Ksz6XiJssjyyoaDnAW6D6nLUIj/AB5EjQC5owho+mOlJ/DherMPWxLE4XBiaFShK";//original 
+			//    string bdProduccion = "Pn6QdbLxN6zYhNuC0AGO9QzP8WL2RI9VHfd/l56YcLkZ1UdzuJNuXq3s7y9ZY3eq6QrxfamnP0GH0FDdEHA6bAWJdHonailm8a5b3eyUw5vuWLyX+mBmFPxKLHFVjRtYm0sjwb1KdqM=";//original 
+			//   string bdDesarrollo = "qbz4h7qjqnp0/OO4YgugGwzP8WL2RI9VHfd/l56YcLkZ1UdzuJNuXhBXIBpMWzG0Ksz6XiJssjyyoaDnAW6D6nLUIj/AB5EjQC5owho+mOlJ/DherMPWxLE4XBiaFShK";//original 
 
-            string bd =  strPublicacion == "Desarrollo" ? bdDesarrollo : strPublicacion == "Prod1" ? bdProduccion : "";
+			string bd = strPublicacion; //strPublicacion == "Desarrollo" ? bdDesarrollo : strPublicacion == "Prod1" ? bdProduccion : "";
 
-            //**********************
-            conexion = new SqlConnection(clsUtiles1.DecryptTripleDES( bd));
+						//**********************
+			conexion = new SqlConnection(clsUtiles1.DecryptTripleDES( bd));
 			
 			 conexion.Open();
 
